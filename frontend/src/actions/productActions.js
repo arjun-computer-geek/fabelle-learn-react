ede0f7af9f9f5f2cfb558a816cee81fff0e1bd94
Arjun <arjun2000raj@gmail.com>
2022-03-29 04:19:29 +0000

    feat : Added Products to products page

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import {
+  ALL_PRODUCTS_REQUEST,
+  ALL_PRODUCTS_SUCCESS,
+  ALL_PRODUCTS_FAIL,
+} from "constants/productContants";
+
+export const getProducts = async (dispatch) => {
+  try {
+    dispatch({ type: ALL_PRODUCTS_REQUEST });
+
+    const { data } = await axios.get("http://localhost:8000/api/products");
+    dispatch({
+      type: ALL_PRODUCTS_SUCCESS,
+      payload: data.products,
+    });
+  } catch (error) {
+    dispatch({
+      type: ALL_PRODUCTS_FAIL,
+      payload: error,
+    });
+  }
+};
