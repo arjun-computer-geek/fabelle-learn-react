1321553a29e69b732a6e2119130c0dde3eb19d17
Arjun <arjun2000raj@gmail.com>
2022-03-30 13:59:46 +0000

    Feat: added alert feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,30 @@
 import React from "react";
-import { 
-  Footer, 
-  Navbar
-} from "components";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Footer, Navbar } from "components";
 
-import { 
-  Route, 
-  Routes 
-} from "react-router-dom";
-import { 
-  Categories, 
-  Home ,
-  Cart
-} from "pages";
+import { Route, Routes } from "react-router-dom";
+import { Categories, Home, Cart } from "pages";
 
 const App = () => {
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
-        <Route path="/cart" element={<Cart />}/>
+        <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
     </>
