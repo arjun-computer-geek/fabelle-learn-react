b76b24bc51d4831a51fb5eec28c86fd81d5f0d86
Arjun <arjun2000raj@gmail.com>
2022-04-04 22:32:17 +0000

    Feat: Loader Added

--- frontend/src/pages/categories/Categories.js
+++ frontend/src/pages/categories/Categories.js
@@ -1,35 +1,41 @@
-import { Card, Sidebar } from "components";
+import { Card, Loader, Sidebar } from "components";
 import { useProduct } from "context/productContext";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
 import "./categories.css";
 
 export const Categories = () => {
-  const { state:{loading, products, error}} = useProduct();
-  useEffect(()=> {
-    if(error){
-      toast.error(error)
+  const {
+    state: { loading, products, error },
+  } = useProduct();
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
     }
-  },[error])
-  
+  }, [error]);
+
   return (
     <>
-      <main className="container">
-        <Sidebar />
-        <div className="main-content">
-          {products.map((product, index) => (
-            <Card
-              key={index}
-              id={product._id}
-              img={`https://fabelle-learn-react.herokuapp.com/public/uploads/${product.image}`}
-              productName={product.name}
-              productOwner={product.owner}
-              noOfReviews={product.numOfReviews}
-              price={product.price}
-            />
-          ))}
-        </div>
-      </main>
+      {loading ? (
+        <Loader />
+      ) : (
+        <main className="container">
+          <Sidebar />
+          <div className="main-content">
+            {products.map((product, index) => (
+              <Card
+                key={index}
+                id={product._id}
+                img={`https://fabelle-learn-react.herokuapp.com/public/uploads/${product.image}`}
+                productName={product.name}
+                productOwner={product.owner}
+                noOfReviews={product.numOfReviews}
+                price={product.price}
+              />
+            ))}
+          </div>
+        </main>
+      )}
     </>
   );
 };
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,59 +1,65 @@
 import React from "react";
 import "./home.css";
 import { categories } from "data/category-data";
-import { Card, CardCategory } from "components";
+import { Card, CardCategory, Loader } from "components";
 import { useProduct } from "context/productContext";
 import { Link } from "react-router-dom";
 export const Home = () => {
   const {
     state: { loading, products },
   } = useProduct();
-  console.log(loading);
-  console.log(products);
   return (
-    <main>
-      <section className="landing-page">
-        <div className="content">
-          <h1>Learning that gets you</h1>
-          <p>Skills for your present (and your future). Get started with us.</p>
-        </div>
-        <div className="represent-img">
-          <img
-            loading="lazy"
-            src="./images/homepage/home.svg"
-            alt="landing-img"
-          />
-        </div>
-      </section>
-
-      <section className="popular-courses">
-        <h2>Popular Courses</h2>
-        <div className="courses-container">
-          {products.slice(0, 10).map((product, index) => (
-            <React.Fragment key={index}>
-              <Link to="/categories">
-              <Card
-                id={product._id}
-                img={`https://fabelle-learn-react.herokuapp.com/public/uploads/${product.image}`}
-                productName={product.name}
-                productOwner={product.owner}
-                noOfReviews={product.numOfReviews}
-                price={product.price}
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <main>
+          <section className="landing-page">
+            <div className="content">
+              <h1>Learning that gets you</h1>
+              <p>
+                Skills for your present (and your future). Get started with us.
+              </p>
+            </div>
+            <div className="represent-img">
+              <img
+                loading="lazy"
+                src="./images/homepage/home.svg"
+                alt="landing-img"
               />
-              </Link>
-            </React.Fragment>
-          ))}
-        </div>
-      </section>
+            </div>
+          </section>
 
-      <section className="top-categories">
-        <h2>Top Categories</h2>
-        <div className="categories-container">
-          {categories.map((item, i) => (
-            <CardCategory key={i} img={item.img} category={item.category} />
-          ))}
-        </div>
-      </section>
-    </main>
+          <section className="popular-courses">
+            <h2>Popular Courses</h2>
+            <div className="courses-container">
+              {products.slice(0, 10).map((product, index) => (
+                <React.Fragment key={index}>
+                  <Link to="/categories">
+                    <Card
+                      id={product._id}
+                      img={`https://fabelle-learn-react.herokuapp.com/public/uploads/${product.image}`}
+                      productName={product.name}
+                      productOwner={product.owner}
+                      noOfReviews={product.numOfReviews}
+                      price={product.price}
+                    />
+                  </Link>
+                </React.Fragment>
+              ))}
+            </div>
+          </section>
+
+          <section className="top-categories">
+            <h2>Top Categories</h2>
+            <div className="categories-container">
+              {categories.map((item, i) => (
+                <CardCategory key={i} img={item.img} category={item.category} />
+              ))}
+            </div>
+          </section>
+        </main>
+      )}
+    </>
   );
 };
