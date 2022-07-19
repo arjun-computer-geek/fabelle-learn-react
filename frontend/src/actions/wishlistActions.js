637c108339e085206acc5aee03640629aed608aa
Arjun <arjun2000raj@gmail.com>
2022-07-19 22:15:43 +0530

    fix: small fix of proxy

--- frontend/src/actions/wishlistActions.js
+++ frontend/src/actions/wishlistActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ADD_TO_WISHLIST, REMOVE_FROM_WISHLIST } from "constants/wishlistConstants";
 
 export const addItemToWishlist = async (id, dispatch) => {
-  const { data } = await axios.get(`https://fabelle-learn-react.herokuapp.com/api/product/${id}`);
+  const { data } = await axios.get(`/api/product/${id}`);
   dispatch({
     type: ADD_TO_WISHLIST,
     payload: data.product,
