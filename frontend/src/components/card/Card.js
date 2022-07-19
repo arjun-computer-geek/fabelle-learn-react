a743a544b006dd25307ef418a88438e3b542f621
Arjun <arjun2000raj@gmail.com>
2022-07-20 02:56:07 +0530

    fix: home page's card height fixed

--- frontend/src/components/card/Card.js
+++ frontend/src/components/card/Card.js
@@ -3,13 +3,15 @@ import { useCart } from 'context/cartContext'
 import { useWishlist } from 'context/wishlistContext'
 import React from 'react'
 
-export const Card = ({id, img,productName, productOwner, noOfReviews, price, ratings}) => {
-    const {addToCart} = useCart()
-    const {addToWishlist} = useWishlist()
-    
+export const Card = ({ id, img, productName, productOwner, noOfReviews, price, ratings }) => {
+    const { addToCart } = useCart()
+    const { addToWishlist } = useWishlist()
+
     return (
         <div className="card">
-            <img className='card-img' loading="lazy" src={img} alt="product" />
+            <div className='card-img'>
+                <img loading="lazy" src={img} alt="product" />
+            </div>
             <div className="card-container">
                 <h4 className="product-name">{productName}</h4>
                 <h3 className="product-owner">{productOwner}</h3>
@@ -17,9 +19,9 @@ export const Card = ({id, img,productName, productOwner, noOfReviews, price, rat
                     <Rating name="half-rating-read" defaultValue={0} value={ratings} precision={0.5} readOnly />
                     <span>({noOfReviews})</span>
                 </div>
-                <div className="product-price">&#8377;<span>{price <= 0? "Free": price}</span></div>
-                <button className="btn product-add-to-cart-btn"onClick={() => addToCart(id)}>Add to cart</button>
-                <button className="product-wish-btn" onClick={() => addToWishlist(id) }>
+                <div className="product-price">&#8377;<span>{price <= 0 ? "Free" : price}</span></div>
+                <button className="btn product-add-to-cart-btn" onClick={() => addToCart(id)}>Add to cart</button>
+                <button className="product-wish-btn" onClick={() => addToWishlist(id)}>
                     <i className="fa fa-heart"></i>
                 </button>
             </div>
